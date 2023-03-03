96ed6712a1e876397027b8bfc57433d77796a1e6
rory <35566455+Thorekt@users.noreply.github.com>
2023-03-03 10:55:35 +0100

    Affichage des bar noires sur les navbar

--- src/components/LeftNavbar.jsx
+++ src/components/LeftNavbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/LeftNavbar.css';
 
 /*
  * This is the left navbar.
--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../styles/TopNavbar.css';
+
 /*
  * This is the top navbar.
  *
