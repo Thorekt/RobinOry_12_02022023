bae98175aff20e3297ea881e285b92de42f69d29
rory <35566455+Thorekt@users.noreply.github.com>
2023-03-03 11:26:03 +0100

    Ajout de commentaire sur top navbar

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -5,7 +5,7 @@ import '../styles/TopNavbar.css';
 import logo from '../assets/icons/icon_logo_sportsee_178x60.93.svg';
 
 /*
- * This is the top navbar.
+ * This is the top navbar. Contains the logo and the links to the other pages.
  *
  * @returns {JSX.Element} The top navbar.
  */
