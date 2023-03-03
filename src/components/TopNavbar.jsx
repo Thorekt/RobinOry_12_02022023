c10df1c7a95a2a9bd1274c8747f51343390ef499
rory <35566455+Thorekt@users.noreply.github.com>
2023-03-03 11:22:11 +0100

    intégration top nav bar

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/TopNavbar.css';
+import logo from '../assets/icons/icon_logo_sportsee_178x60.93.svg';
 
 /*
  * This is the top navbar.
@@ -8,7 +10,17 @@ import '../styles/TopNavbar.css';
  * @returns {JSX.Element} The top navbar.
  */
 function TopNavbar() {
-  return <div className='top-navbar'></div>;
+  return (
+    <nav className='top-navbar'>
+      <div className='top-navbar__logo'>
+        <img src={logo} alt='SportSee' />
+      </div>
+      <Link>Accueil</Link>
+      <Link>Profil</Link>
+      <Link>Réglage</Link>
+      <Link>Communauté</Link>
+    </nav>
+  );
 }
 
 export default TopNavbar;
