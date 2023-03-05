d1b9bb0cd0c02b3b575f0d0f6819db1fd50c8d3a
rory <35566455+Thorekt@users.noreply.github.com>
2023-03-05 22:29:01 +0100

    Left navbar + correction top navbar

--- src/components/LeftNavbar.jsx
+++ src/components/LeftNavbar.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/LeftNavbar.css';
+import icon1 from '../assets/icons/icon_left_navbar_1_64x64.svg';
+import icon2 from '../assets/icons/icon_left_navbar_2_64x64.svg';
+import icon3 from '../assets/icons/icon_left_navbar_3_64x64.svg';
+import icon4 from '../assets/icons/icon_left_navbar_4_64x64.svg';
 
 /*
  * This is the left navbar.
@@ -7,7 +12,25 @@ import '../styles/LeftNavbar.css';
  * @returns {JSX.Element} The left navbar.
  */
 function LeftNavbar() {
-  return <div className='left-navbar'></div>;
+  return (
+    <div className='left-navbar'>
+      <div className='left-navbar-items'>
+        <Link>
+          <img src={icon1} alt='Icon médiation.' />
+        </Link>
+        <Link>
+          <img src={icon2} alt='Icon natation' />
+        </Link>
+        <Link>
+          <img src={icon3} alt='Icon cyclisme' />
+        </Link>
+        <Link>
+          <img src={icon4} alt='Icon haltere' />
+        </Link>
+      </div>
+      <small>Copiryght, SportSee 2020</small>
+    </div>
+  );
 }
 
 export default LeftNavbar;
